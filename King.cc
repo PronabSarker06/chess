dfc4c85c29b20614191f4e4aa2e0391775a66107
a223xu <a223xu@uwaterloo.ca>
2025-07-21 18:30:54 -0400

    King legalmove test2

--- King.cc
+++ King.cc
@@ -12,23 +12,24 @@ bool King::canMove(Move) {
 }
 
 std::vector<Move> King::getLegalMoves() {
-
-    std::vector<Move> result;
-
-    // set colour of the King
-    this->colour == 'b' ? -1 : 1;
-
-    // check each available move. Kings can move at most 1 vertically + 1 horizontally
-    for (int col_change = -1; col_change <= 1; ++col_change) {
-        for (int row_change = -1; row_change <= 1; ++row_change) {
-            Position to = {this->position.getCol() + col_change, this->position.getRow() + row_change};
-            // Destination must be valid, cannot capture its own, and not step into check
-            if (to.valid() && this->colour !=  bptr->grid[to.to1D()]->colour) {
-                Move move = {this->position, to, this, bptr->grid[to.to1D()]};
-                result.emplace_back(move);
-            }
-        }
-    }
-    return result;
-
+   std::vector<Move> result;
+  
+   for (int col_change = -1; col_change <= 1; ++col_change) {
+       for (int row_change = -1; row_change <= 1; ++row_change) {
+           // cannot make a move to original position
+           if (col_change == 0 && row_change == 0) continue;
+           Position to = {
+               this->position.getCol() + col_change,
+               this->position.getRow() + row_change
+           };
+           if (!to.valid()) continue;
+           // Check if square is empty or contains opponent's piece
+           Piece* target = bptr->getPieceAt(to);
+           // Destination can only be empty or an enemy piece
+           if (target == nullptr || this->colour != target->getColour()) {
+               result.emplace_back(this->position, to, this, target);
+           }
+       }
+   }
+   return result;
 }
