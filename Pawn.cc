93f9da88528c5596a5e97777c35f93316b6cc90a
Pronab Sarker <psarker@uwaterloo.ca>
2025-07-22 15:07:01 -0400

    bogo

--- Pawn.cc
+++ Pawn.cc
@@ -22,34 +22,82 @@ std::vector<Move> Pawn::getLegalMoves() {
 
     //moving forward by 1
     //if nothing blocking the pawn
-    Position f1pos = {position.getCol(), position.getRow() + 1 * colourFactor};
+    Position f1pos = {position.getCol(), position.getRow() + 1 * colourFactor}; // add promo
     if (f1pos.valid() && bptr->getDisplayGrid()[f1pos.to1D()] == '0'){
-        Move forward1 = {position, f1pos, this, nullptr, '0'};
-        result.emplace_back(forward1);
+        if (position.getRow() == (colour == 'b' ? 1 : 6)) {
+            Move knightPromo = {position, f1pos, this, nullptr, 'n'}; // promo moves
+            Move bishopPromo = {position, f1pos, this, nullptr, 'b'}; 
+            Move rookPromo = {position, f1pos, this, nullptr, 'r'}; 
+            Move queenPromo = {position, f1pos, this, nullptr, 'q'}; 
+
+            result.emplace_back(knightPromo);
+            result.emplace_back(bishopPromo);
+            result.emplace_back(rookPromo);
+            result.emplace_back(queenPromo);
+        } else { 
+            Move forward1 = {position, f1pos, this, nullptr, '0'};
+            result.emplace_back(forward1);
+        }
     }
     
     //try moving up 2
     //only if first move, can go up 2, check nothing blocks
-    Position f2pos = {position.getCol(), position.getRow() + 2 * colourFactor};
+    Position f2pos = {position.getCol(), position.getRow() + 2 * colourFactor}; // add promo (setup can have a pawn promote on first move)
     if (!hasMoved && f2pos.valid() && bptr->getDisplayGrid()[f2pos.to1D()] == '0'){
-        Move forward2 = {position, f2pos, this, nullptr, '0'};
-        result.emplace_back(forward2);
+        if (position.getRow() == (colour == 'b' ? 6 : 1)) {
+            Move knightPromo = {position, f2pos, this, nullptr, 'n'}; // promo moves
+            Move bishopPromo = {position, f2pos, this, nullptr, 'b'}; 
+            Move rookPromo = {position, f2pos, this, nullptr, 'r'}; 
+            Move queenPromo = {position, f2pos, this, nullptr, 'q'}; 
+
+            result.emplace_back(knightPromo);
+            result.emplace_back(bishopPromo);
+            result.emplace_back(rookPromo);
+            result.emplace_back(queenPromo);
+        } else { 
+            Move forward2 = {position, f2pos, this, nullptr, '0'};
+            result.emplace_back(forward2);
+        }
     }
 
     // cap left
-    Position clpos = {position.getCol() - 1, position.getRow() + 1 * colourFactor};
+    Position clpos = {position.getCol() - 1, position.getRow() + 1 * colourFactor}; // add promo
     Piece* p = bptr->getPieceAt(clpos);
     if (p && p->getColour() != colour) {
-        Move capleft = {position, clpos, this, p, '0'};
-        result.emplace_back(capleft);
+        if (position.getRow() == (colour == 'b' ? 2 : 5)) {
+            Move knightPromo = {position, clpos, this, nullptr, 'n'}; // promo moves
+            Move bishopPromo = {position, clpos, this, nullptr, 'b'}; 
+            Move rookPromo = {position, clpos, this, nullptr, 'r'}; 
+            Move queenPromo = {position, clpos, this, nullptr, 'q'}; 
+
+            result.emplace_back(knightPromo);
+            result.emplace_back(bishopPromo);
+            result.emplace_back(rookPromo);
+            result.emplace_back(queenPromo);
+        } else { 
+            Move capleft = {position, clpos, this, p, '0'};
+            result.emplace_back(capleft);
+        }
     }
 
     //cap right
-    Position crpos = {position.getCol() + 1, position.getRow() + 1 * colourFactor}; // good cap checks
+    Position crpos = {position.getCol() + 1, position.getRow() + 1 * colourFactor}; // add promo
     p = bptr->getPieceAt(crpos);
     if (p && p->getColour() != colour) {
-        Move capright = {position, crpos, this, p, '0'};
-        result.emplace_back(capright);
+        if (position.getRow() == (colour == 'b' ? 1 : 6)) {
+            Move knightPromo = {position, crpos, this, nullptr, 'n'}; // promo moves
+            Move bishopPromo = {position, crpos, this, nullptr, 'b'}; 
+            Move rookPromo = {position, crpos, this, nullptr, 'r'}; 
+            Move queenPromo = {position, crpos, this, nullptr, 'q'}; 
+
+            result.emplace_back(knightPromo);
+            result.emplace_back(bishopPromo);
+            result.emplace_back(rookPromo);
+            result.emplace_back(queenPromo);
+        } else { 
+            Move capright = {position, crpos, this, p, '0'};
+            result.emplace_back(capright);
+        }
     }
     
     return result;
