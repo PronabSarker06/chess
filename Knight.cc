f175f093a287fbef00cf5e253a050469218064b1
a223xu <a223xu@uwaterloo.ca>
2025-07-22 01:18:01 -0400

    Knight

--- Knight.cc
+++ Knight.cc
@@ -1,5 +1,7 @@
 #include "Knight.h"
 #include "Move.h"
+#include "Board.h"
+#include <vector>
 
 Knight::Knight(char c, Position p, Board* bptr) 
     : Piece(c, 'n', p, bptr) {}
@@ -10,10 +12,28 @@ bool Knight::canMove(Move) {
     
 }
 
+
 std::vector<Move> Knight::getLegalMoves() {
-
     std::vector<Move> result;
-
+    // Check all 8 possible moves
+    std::vector<int> col_change = {2, 2, -2, -2, 1, 1, -1, -1};
+    std::vector<int> row_change = {1, -1, 1, -1, 2, -2, 2, -2};
+    for (int i = 0; i < 8; ++i) {
+        Position to = {
+            position.getCol() + col_change.at(i),
+            position.getRow() + row_change.at(i)
+        };
+        if (to.valid()) {
+            // Check if destination is empty
+            if (bptr->getDisplayGrid()[to.to1D()] == '0') {
+                result.push_back(Move{position, to, this, nullptr});
+            }
+            // Check if destination is an enemy piece
+            else if (bptr->getPieceAt(to) && 
+            this->colour != bptr->getPieceAt(to)->getColour()) {
+                result.push_back(Move{position, to, this, bptr->getPieceAt(to)});
+            }
+        }
+    }
     return result;
-
 }
